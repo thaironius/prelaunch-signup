bbaa9cb457940508ae1469a7a527c0ba70a538bc
Thaironius <thaironius@gmail.com>
2013-10-25 09:36:39 -0700

    implement 'Request Invitation' feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,38 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
-  
+  # override Devise method
+  # no password is required when the account is created; validates password when the user sets one
+  validates_confirmation_of :password
+  def password_required?
+    if !persisted?
+      !(password != "")
+    else
+      !password.nil? || !password_confirmation.nil?
+    end
+  end
+
+  # overide Devise method
+  def confirmation_required?
+  	false
+  end
+
+  #override Devise method
+  def active_for_authentication?
+  	confirmed? || confirmation_period_valid?
+  end
+
+  def send_reset_password_instructions
+  	if self.confirmed?
+  		super
+  	else
+  		errors.add :base, "You must receive an invitation before you set your password."
+  	end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ RailsPrelaunchSignup::Application.routes.draw do
   end
   devise_scope :user do
     root :to => "devise/registrations#new"
-    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
-  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
-  match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
-  resources :users do
-    get 'invite', :on => :member
-  end
+
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  resources :users
 end
